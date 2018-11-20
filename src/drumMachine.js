32ecc911df9a63228efa6673cde94c5fdc3bc2a6
Abdeladim Saouiss <abdeladim.saouiss@gmail.com>
2018-11-20 02:14:11 +0100

    Drum pads is working with both click & keydown event. Also The display is updated with the right drum name

--- src/drumMachine.js
+++ src/drumMachine.js
@@ -5,24 +5,33 @@ import './App.css';
 // const sample = './start-of-a-new-day.mp3'
 
 let drumpadData = [
-  {id: "Q", song: "http://files.sampulator.com/sounds/claps/normal3.mp3"},
-  {id: "W", song: "http://files.sampulator.com/sounds/snare/snare-huge-reverb.mp3"},
-  {id: "E", song: "http://files.sampulator.com/sounds/kicks/punchy-trap.mp3"},
-  {id: "A", song: "http://files.sampulator.com/sounds/snare/snare-rim-real.mp3"},
-  {id: "S", song: "https://s3.amazonaws.com/freecodecamp/drums/Heater-6.mp3"},
-  {id: "D", song: "http://files.sampulator.com/sounds/808/8084.mp3"},
-  {id: "Z", song: "http://files.sampulator.com/sounds/perc/triangle.mp3"},
-  {id: "X", song: "http://files.sampulator.com/sounds/perc/tom.mp3"},
-  {id: "C", song: "http://files.sampulator.com/sounds/cymbals/crash2.mp3"},
+  {id: "Q", name:"Claps", song: "http://files.sampulator.com/sounds/claps/normal3.mp3"},
+  {id: "W", name:"Snare huge", song: "http://files.sampulator.com/sounds/snare/snare-huge-reverb.mp3"},
+  {id: "E", name:"kicks", song: "http://files.sampulator.com/sounds/kicks/punchy-trap.mp3"},
+  {id: "A", name:"Snare rim", song: "http://files.sampulator.com/sounds/snare/snare-rim-real.mp3"},
+  {id: "S", name:"Heater", song: "https://s3.amazonaws.com/freecodecamp/drums/Heater-6.mp3"},
+  {id: "D", name:"Dom", song: "http://files.sampulator.com/sounds/808/8084.mp3"},
+  {id: "Z", name:"triangle", song: "http://files.sampulator.com/sounds/perc/triangle.mp3"},
+  {id: "X", name:"Tom", song: "http://files.sampulator.com/sounds/perc/tom.mp3"},
+  {id: "C", name:"crash", song: "http://files.sampulator.com/sounds/cymbals/crash2.mp3"},
 ]
 
 
 class Drumpad extends Component {
   render() {
     return (
-      <div className="drum-pad" id={this.props.id} onClick={this.props.onClick}>
-        <audio className="audio" src={this.props.song}></audio>
+      <div className="drum-pad" onClick={this.props.onClick} data-name={this.props.name}>
+        <audio className="clip" src={this.props.song} id={this.props.id}></audio>
+        <span>{this.props.id}</span>
       </div>
+    );
+  }
+}
+
+class Display extends Component {
+  render() {
+    return (
+      <div id="display" className="display">{this.props.drumName}</div>
     );
   }
 }
@@ -33,10 +42,11 @@ class DrumMachine extends Component {
     super(props);
 
     this.state = {
-      drumpads: drumpadData
+      drumpads: drumpadData,
+      drumName: ""
     };
 
-    // this.handleClick = this.handleClick.bind(this)
+    // this.handleClick = this.handleClick.bind(this); // Don't need this if use arrow function
   }
 
   componentDidMount() {
@@ -48,31 +58,42 @@ class DrumMachine extends Component {
 
   // Play the drum sample
   playDrum = elem => {
-    let drumSample = elem.childNodes[0];
+    let drumSample = elem;
     drumSample.pause();
     drumSample.currentTime = 0;
     drumSample.play();
   };
 
+  // Drup pad click handler
   handleClick = e => {
-    // // console.log("Event type : ", e.type);
-    // console.log("Event : ", e.target);
-    // // console.log(e.type);
-    this.playDrum(e.target);
+    this.playDrum(e.target.childNodes[0]);
+    this.setState({
+      drumName: e.target.dataset.name
+    })
   };
   
+  // Keydown handler
   handleKeyDown = (e) => {
-    // console.log("Event : ", e.key.toUpperCase());
+    /**
+     * Get the content of the right clicked keyboard key in filtered variable
+     * by comparing the event.key to the id of this.state.drumpads
+     */
     let filtered = this.state.drumpads.filter(drumpad => drumpad.id === e.key.toUpperCase());
+    /**
+     * Make sure that filtered variable contains one explicit item and do the logic
+     */
     if(filtered.length !== 0){
       this.playDrum(document.getElementById(filtered[0].id));
+      this.setState({
+        drumName: filtered[0].name
+      })
     }    
   };
 
   render() {
     return (
       <div className="drum-machine" id="drum-machine">
-        <div id="display" />
+        <Display drumName={this.state.drumName} />
         <div className="drum-pads">
           {this.state.drumpads.map((drumpad, i) => (
             <Drumpad
@@ -80,6 +101,7 @@ class DrumMachine extends Component {
               key={`${drumpad.id}${i}`}
               song={drumpad.song}
               onClick={this.handleClick}
+              name={drumpad.name}
             />
           ))}
         </div>
