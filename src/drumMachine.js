ca832d7aea3c1de10fa5314137fdcbc742538e68
Abdeladim Saouiss <abdeladim.saouiss@gmail.com>
2018-12-10 16:13:49 +0100

    Both keyboard and mouse handlers are working

--- src/drumMachine.js
+++ src/drumMachine.js
@@ -3,28 +3,45 @@ import React, { Component } from 'react';
 import './App.css';
 
 // const sample = './start-of-a-new-day.mp3'
+const drumStyle = {
+  boxShadow: "0px 3px 1px 4px rgba(255, 45, 45, 0.8)"
+};
+
+const drumActiveStyle = {
+  transform: "translateY(3px)",
+  boxShadow: "0px 1px 1px 2px rgba(255, 45, 45, 0.8)"
+};
 
 let drumpadData = [
-  {id: "Q", name:"Claps", song: "http://files.sampulator.com/sounds/claps/normal3.mp3"},
-  {id: "W", name:"Snare huge", song: "http://files.sampulator.com/sounds/snare/snare-huge-reverb.mp3"},
-  {id: "E", name:"kicks", song: "http://files.sampulator.com/sounds/kicks/punchy-trap.mp3"},
-  {id: "A", name:"Snare rim", song: "http://files.sampulator.com/sounds/snare/snare-rim-real.mp3"},
-  {id: "S", name:"Heater", song: "https://s3.amazonaws.com/freecodecamp/drums/Heater-6.mp3"},
-  {id: "D", name:"Dom", song: "http://files.sampulator.com/sounds/808/8084.mp3"},
-  {id: "Z", name:"triangle", song: "http://files.sampulator.com/sounds/perc/triangle.mp3"},
-  {id: "X", name:"Tom", song: "http://files.sampulator.com/sounds/perc/tom.mp3"},
-  {id: "C", name:"crash", song: "http://files.sampulator.com/sounds/cymbals/crash2.mp3"},
+  {index: 0 ,id: "Q", name:"Claps", style: drumStyle, song: "http://files.sampulator.com/sounds/claps/normal3.mp3"},
+  {index: 1 ,id: "W", name:"Snare huge", style: drumStyle, song: "http://files.sampulator.com/sounds/snare/snare-huge-reverb.mp3"},
+  {index: 2 ,id: "E", name:"kicks", style: drumStyle, song: "http://files.sampulator.com/sounds/kicks/punchy-trap.mp3"},
+  {index: 3 ,id: "A", name:"Snare rim", style: drumStyle, song: "http://files.sampulator.com/sounds/snare/snare-rim-real.mp3"},
+  {index: 4 ,id: "S", name:"Heater", style: drumStyle, song: "https://s3.amazonaws.com/freecodecamp/drums/Heater-6.mp3"},
+  {index: 5 ,id: "D", name:"Dom", style: drumStyle, song: "http://files.sampulator.com/sounds/808/8084.mp3"},
+  {index: 6 ,id: "Z", name:"triangle", style: drumStyle, song: "http://files.sampulator.com/sounds/perc/triangle.mp3"},
+  {index: 7 ,id: "X", name:"Tom", style: drumStyle, song: "http://files.sampulator.com/sounds/perc/tom.mp3"},
+  {index: 8 ,id: "C", name:"crash", style: drumStyle, song: "http://files.sampulator.com/sounds/cymbals/crash2.mp3"},
 ]
 
 
 class Drumpad extends Component {
+  componentDidMount() {
+    document.addEventListener("keydown", this.props.handleKeyDown, false);
+    document.addEventListener("keyup", this.props.handleKeyUp, false);
+  }
+  componentWillUnmount() {
+    document.removeEventListener("keydown", this.props.handleKeyDown, false);
+    document.addEventListener("keyup", this.props.handleKeyUp, false);
+  }
+
   render() {
     return (
-      <div className="drum-pad" onClick={this.props.onClick} data-name={this.props.name}>
-        <audio className="clip" src={this.props.song} id={this.props.id}></audio>
-        <span>{this.props.id}</span>
+      <div className="drum-pad" onMouseDown={this.props.mouseDown} onMouseUp={this.props.mouseUp} data-name={this.props.drumpad.name} data-index={this.props.drumpad.index} style={this.props.drumpad.style}>
+        <audio className="clip" src={this.props.drumpad.song} id={this.props.drumpad.id} />
+        <span>{this.props.drumpad.id}</span>
       </div>
-    );
+    )
   }
 }
 
@@ -43,17 +60,11 @@ class DrumMachine extends Component {
 
     this.state = {
       drumpads: drumpadData,
-      drumName: ""
+      drumName: "",
+      drumStyle: drumStyle
     };
 
     // this.handleClick = this.handleClick.bind(this); // Don't need this if use arrow function
-  }
-
-  componentDidMount() {
-    document.addEventListener("keydown", this.handleKeyDown, false);
-  }
-  componentWillUnmount() {
-    document.removeEventListener("keydown", this.handleKeyDown, false);
   }
 
   // Play the drum sample
@@ -65,48 +76,74 @@ class DrumMachine extends Component {
   };
 
   // Drup pad click handler
-  handleClick = e => {
+  handleMouseDown = e => {
+    let drumpads = this.state.drumpads;
+    drumpads[e.target.dataset.index].style = drumActiveStyle;
     this.playDrum(e.target.childNodes[0]);
     this.setState({
+      drumpads: drumpads,
       drumName: e.target.dataset.name
-    })
+    });
   };
-  
+
+  handleMouseUp = e => {
+    let drumpads = this.state.drumpads;
+    drumpads[e.target.dataset.index].style = drumStyle;
+    this.setState({
+      drumpads: drumpads,
+    });
+  }
+
   // Keydown handler
-  handleKeyDown = (e) => {
+  handleKeyDown = e => {
     /**
      * Get the content of the right clicked keyboard key in filtered variable
      * by comparing the event.key to the id of this.state.drumpads
      */
-    let filtered = this.state.drumpads.filter(drumpad => drumpad.id === e.key.toUpperCase());
+    let filtered = this.state.drumpads.filter(
+      drumpad => drumpad.id === e.key.toUpperCase()
+    );
     /**
      * Make sure that filtered variable contains one explicit item and do the logic
      */
-    if(filtered.length !== 0){
+    if (filtered.length !== 0) {
+      let drumpads = this.state.drumpads;
+      drumpads[filtered[0].index].style = drumActiveStyle;
       this.playDrum(document.getElementById(filtered[0].id));
       this.setState({
+        drumpads: drumpads,
         drumName: filtered[0].name
-      })
-    }    
+      });
+    }
   };
 
+  handleKeyUp = e => {
+    let filtered = this.state.drumpads.filter(drumpad => drumpad.id === e.key.toUpperCase());
+    if (filtered.length !== 0) {
+      let drumpads = this.state.drumpads;
+      drumpads[filtered[0].index].style = drumStyle;
+      this.setState({
+        drumpads: drumpads,
+      });
+    }
+  }
+
   render() {
-    return (
-      <div className="drum-machine" id="drum-machine">
+    return <div className="drum-machine" id="drum-machine">
         <Display drumName={this.state.drumName} />
         <div className="drum-pads">
           {this.state.drumpads.map((drumpad, i) => (
             <Drumpad
-              id={drumpad.id}
               key={`${drumpad.id}${i}`}
-              song={drumpad.song}
-              onClick={this.handleClick}
-              name={drumpad.name}
+              drumpad={drumpad}
+              mouseDown={this.handleMouseDown}
+              mouseUp={this.handleMouseUp}
+              handleKeyDown={this.handleKeyDown}
+              handleKeyUp={this.handleKeyUp}
             />
           ))}
         </div>
-      </div>
-    );
+      </div>;
   }
 }
 
